5dbb9d363a2f06e8a45a210c6715b20d42ca3698
Schlepp Krisztián <krisztian.schlepp@gmail.com>
2022-04-19 17:14:54 +0200

    refactor: move sproutsML

---
+++ SproutsML/__init__.py
@@ -0,0 +1,4 @@
+# shared object is added during setup
+import sprouts  # type:ignore
+
+__all__ = ["sprouts"]
--- setup.py
+++ setup.py
@@ -4,5 +4,7 @@ setup(
     name="SporutsML",
     packages=["SproutsML"],
     package_dir={"SproutsML": "SproutsML"},
-    package_data={"SproutsML": ["py.typed"]},
+    package_data={
+        "SproutsML": ["py.typed", "build/sprouts.cpython-39-x86_64-linux-gnu.so"]
+    },
 )
